8b39fa8b9861ab82c5e4831cb41a245155a3d312
AceSCav <aleff.cavalcante.09@gmail.com>
2025-04-23 14:49:47 +0100

    Added new functions

--- FastaToNexus.py
+++ FastaToNexus.py
@@ -1,7 +1,5 @@
 import sys
 
-with open("C:/Users/alfca/OneDrive/Desktop/Estudos/Python/small_seq.txt") as file:
-    file = file.readlines()
 def FastaToDict(file):
     seq_dict = {}
     for line in file:
@@ -16,6 +14,7 @@ def NexusHeader(seq_dict):
     seq_length = len(list(seq_dict.values())[0])
     num_seqs = len(seq_dict)
     header = "#NEXUS\n"
+    header += "\n"
     header += "BEGIN DATA;\n"
     header += f"DIMENSIONS NTAX={num_seqs} NCHAR={seq_length};\n"
     header += "FORMAT DATATYPE=DNA MISSING=N GAP=-;\n"
@@ -26,9 +25,19 @@ def NexusMatrix(seq_dict):
     matrix = ""
     for seq_name, seq in seq_dict.items():
         matrix += f"{seq_name[1::]} {seq}\n"
-    matrix += ";\n"
+    matrix += ";\nEND;\n"
     return matrix
 
-seq_dict = FastaToDict(file)
-print(NexusHeader(seq_dict))
-print(NexusMatrix(seq_dict))
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        in_file = sys.argv[1]
+        with open(in_file, "r") as file:
+            seq_dict = FastaToDict(file)
+            header = NexusHeader(seq_dict)
+            matrix = NexusMatrix(seq_dict)
+            print(header + matrix)
+    else:
+        print("Usage: python FastaToNexus.py file.fasta")
+        sys.exit(1)
+
+
