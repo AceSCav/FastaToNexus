302e4e02b5757738dd89b50ef544680eb19b580b
Alex112004 <123894085+Alex112004@users.noreply.github.com>
2025-05-06 15:16:37 +0100

    documentação do código FastaToNexus.py

--- FastaToNexus.py
+++ FastaToNexus.py
@@ -1,6 +1,17 @@
 import sys
 
 def FastaToDict(file):
+    """
+    Converte o ficheiro FASTA em um dicionário com as sequências que este contem.
+
+    Cada linha identificada o incio com '>' que antecede de cada sequencia para o dicionário até existe outro '>'
+
+    Args:
+        file (iteravel): ficheiro que contem dados no formato FASTA.
+
+    Returns:
+        dict: O dicionário que as chaves são os nomes das sequências e os valores são as sequências.
+    """
     seq_dict = {}
     for line in file:
         if line.startswith(">"):
@@ -11,6 +22,20 @@ def FastaToDict(file):
     return seq_dict 
 
 def NexusHeader(seq_dict):
+    """
+    cria o cabeçalho do ficheiro NEXUS a partir do dicionário de sequências que foi criada na funcao 'FastaToDict'.
+
+    O cabeçalho inclui a estrutura obrigatória do bloco `BEGIN DATA`, com as dimensões sendo estas número de sequências e das mesmas comprimento das sequências,
+    o formato de dados de DNA, que utiliza 'N' para os carácter que nao existem e '-' para os gap, e inicia a seção `MATRIX` onde as sequências serão inceridas
+    no ficehiro.
+
+    Args:
+        seq_dict (dict): O dicionário que as chaves são cabeçalhos de sequência (ex: '>seq1') e os valores são as sequências a que os chaves correspondem 
+                         (ex: 'TAAGC...').
+
+    Returns:
+        str: String formatada que contem o cabeçalho do ficheiro NEXUS pronto para ser escrito.
+    """
     seq_length = len(list(seq_dict.values())[0])
     num_seqs = len(seq_dict)
     header = "#NEXUS\n"
@@ -22,6 +47,18 @@ def NexusHeader(seq_dict):
     return header
 
 def NexusMatrix(seq_dict):
+    """
+    Cria a parte MATRIX do ficheiro NEXUS a partir do dicionário das sequências.
+
+    Para cada chave do dicionário, o nome da sequência sem o '>' é alinhado com a sequência correspondente. A estrutura restringe-se ao padrão do bloco MATRIX
+    do formato NEXUS.
+
+    Args:
+        seq_dict (dict): Dicionário com chaves que sao os nomes das sequências (ex: '>seq1') e os valores que sao as sequências de nucleótidos (ex: 'TAAGC...').
+
+    Returns:
+        str: Uma string que contem as sequências no bloco MATRIX, que termina com ';' e 'END;'.
+    """
     matrix = ""
     for seq_name, seq in seq_dict.items():
         matrix += f"{seq_name[1::]} {seq}\n"
